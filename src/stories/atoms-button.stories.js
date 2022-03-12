2bcc33d8d074e50e6cb2d7ba9c6424429542dc67
murasige_ryosuke <r.murasige4nonome.com>
2022-03-12 22:25:07 +0900

    buttonの作成　変数ファイルの移動

---
+++ src/stories/atoms-button.stories.js
@@ -0,0 +1,6 @@
+import Item_Example from "../components/atoms/button/button.html";
+import "../components/atoms/button/button.scss";
+
+export default { title: "Atoms/Buttton" }
+
+export const Example = () => Item_Example;
