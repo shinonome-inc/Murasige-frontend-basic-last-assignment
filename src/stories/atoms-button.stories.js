3b61e2e7ba171233e19aecb442e6d661ac88acca
murasige_ryosuke <r.murasige4nonome.com>
2022-03-13 23:00:14 +0900

    button.scssを書き加えました

--- src/stories/atoms-button.stories.js
+++ src/stories/atoms-button.stories.js
@@ -1,6 +1,6 @@
-import Item_Example from "../components/atoms/button/button.html";
+import Item_Button from "../components/atoms/button/button.html";
 import "../components/atoms/button/button.scss";
 
 export default { title: "Atoms/Buttton" }
 
-export const Example = () => Item_Example;
+export const Button = () => Item_Button;
